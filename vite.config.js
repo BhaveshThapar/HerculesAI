c9930fc37a8505a9041c1bd88e0a4ba99bfe5b63
Bhavesh <s.thapar1212@gmail.com>
2025-07-05 02:53:10 -0400

    Commit

--- vite.config.js
+++ vite.config.js
@@ -2,20 +2,18 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
 
+
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss()
   ],
-  build: {
-    outDir: 'dist',
-    sourcemap: false,
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          vendor: ['react', 'react-dom'],
-          router: ['react-router-dom']
-        }
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:5001',
+        changeOrigin: true,
+        secure: false,
       }
     }
   }
